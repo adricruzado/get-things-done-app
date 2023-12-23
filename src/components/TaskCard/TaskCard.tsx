7ffd20c6b4b3a613526046ae4c431118d44a9048
Adri Cruzado <adricruzado96@gmail.com>
2023-12-23 13:24:08 +0100

    Add toggleDone function

--- src/components/TaskCard/TaskCard.tsx
+++ src/components/TaskCard/TaskCard.tsx
@@ -2,15 +2,22 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import { Task } from "../../types";
+import { MouseEventHandler } from "react";
 
 interface TaskCardProps {
   task: Task;
+  toggleDone: (taskId: string) => MouseEventHandler<HTMLParagraphElement>;
 }
 
-const TaskCard = ({ task }: TaskCardProps): React.ReactElement => {
+const TaskCard = ({ task, toggleDone }: TaskCardProps): React.ReactElement => {
   return (
     <div className="task">
-      <p>{task.description}</p>
+      <p
+        onClick={toggleDone(task.id)}
+        className={`${task.isDone ? "done" : ""}`}
+      >
+        {task.description}
+      </p>
       <div>
         <FontAwesomeIcon icon={faPenToSquare} />
         <FontAwesomeIcon icon={faTrash} />
