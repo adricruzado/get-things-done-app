80426fc280e30c2bfe3599795d6802840711223c
Adri Cruzado <adricruzado96@gmail.com>
2023-12-23 13:24:26 +0100

    Update TaskCard component test

--- src/components/TaskCard/TaskCard.test.tsx
+++ src/components/TaskCard/TaskCard.test.tsx
@@ -3,14 +3,17 @@ import "@testing-library/jest-dom";
 import TaskCard from "./TaskCard";
 import { taskMock } from "../../mocks/taskMock";
 import { Task } from "../../types";
+import { vi } from "vitest";
 
 describe("Given a TaskCard component", () => {
+  const toggleDone = vi.fn();
+
   describe("When it is rendered", () => {
     test("Then it should show a task's description", () => {
       const task: Task = taskMock;
       const expectedDescription = taskMock.description;
 
-      render(<TaskCard task={task} />);
+      render(<TaskCard toggleDone={toggleDone} task={task} />);
 
       const description = screen.getByText(expectedDescription);
 
